9905ad622ce8f13c72ff0d6ab5d3d00ae46f6f33
angelorapana <angelorapana@icloud.com>
2026-02-09 21:13:22 +0100

    creazione header con logo e navbar con metodo map (no css)

--- dcCOMICS/src/components/Header.jsx
+++ dcCOMICS/src/components/Header.jsx
@@ -1,23 +1,37 @@
 const links = [
-    { name: 'Home', href: '#' },
-    { name: 'Characters', href: '#' },
-    { name: 'Comics', href: '#' },
-    { name: 'Movies', href: '#' },
-]
+    { id: 1, text: "CHARACTERS", url: '#', current: false },
+    { id: 2, text: 'COMICS', url: '#', current: true },
+    { id: 3, text: 'MOVIES', url: '#', current: false },
+    { id: 4, text: 'TV', url: '#', current: false },
+    { id: 5, text: 'GAMES', url: '#', current: false },
+    { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
+    { id: 7, text: 'VIDEOS', url: '#', current: false },
+    { id: 8, text: 'FANS', url: '#', current: false },
+    { id: 9, text: 'NEWS', url: '#', current: false },
+    { id: 10, text: 'SHOP', url: '#', current: false },
+];
 
-function nav() {
+function Nav() {
     return (
-        links.map(link => (
-            <li key={link.name}>
-                <a href={link.href}>{link.name}</a>
-            </li>
-        )))
+        <nav>
+            <ul>
+                {links.map(link => (
+                    <li key={link.id} className={link.current ? 'current' : ''}>
+                        <a href={link.url}>{link.text}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
+
 
 export default function Header() {
     return (
         <header className="header">
-            <img src="/dc-logo.png" alt="DC Comics Logo" className="logo" />
+            <img src="img/dc-logo.png" alt="DC Comics Logo" className="logo" />
+            <Nav />
         </header>
     )
 }
+
