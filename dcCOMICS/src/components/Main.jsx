a17b4a72bd7ab45e2f04f9f7b59a04d87974fb25
angelorapana <angelorapana@icloud.com>
2026-02-10 00:31:46 +0100

    creazione sezioni

--- dcCOMICS/src/components/Main.jsx
+++ dcCOMICS/src/components/Main.jsx
@@ -7,29 +7,108 @@ const info = [
     { id: 4, text: "COMIC SHOP LOCATOR", img: "/img/buy-comics-shop-locator.png" },
     { id: 5, text: "DC POWER VISA", img: "/img/buy-dc-power-visa.svg" },
 ];
-
 function Info() {
     return (
-        <div className="container">
-            <ul>
-                {info.map(item => (
-                    <li key={item.id}>
-                        <img src={item.img} /> {item.text}
-                    </li>
-                ))}
-            </ul>
-        </div >
-    )
+        <div className="bg-primary py-4">
+            <div className="container">
+                <ul className="list-unstyled d-flex justify-content-between align-items-center mb-0">
+                    {info.map(item => (
+                        <li
+                            key={item.id}
+                            className="d-flex align-items-center gap-2 text-white"
+                        >
+                            <img
+                                src={item.img}
+                                alt={item.text}
+                                style={{ height: "40px" }}
+                            />
+                            <span className="fw-semibold small">{item.text}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
+
+const colonne = [
+    {
+        id: 1,
+        titolo: "DC COMICS",
+        sezioni: [
+            "Characters",
+            "Comics",
+            "Movies",
+            "TV",
+            "Games",
+            "Videos",
+            "News",
+        ],
+    },
+    {
+        id: 2,
+        titolo: "DC",
+        sezioni: [
+            "Terms Of Use",
+            "Privacy Policy (New)",
+            "Ad Choices",
+            "Advertising",
+            "Jobs",
+            "Subscriptions",
+            "Talent Workshops",
+        ],
+    },
+    {
+        id: 3,
+        titolo: "SITES",
+        sezioni: [
+            "DC",
+            "MAD Magazine",
+            "DC Kids",
+            "DC Universe",
+            "DC Power Visa",
+        ],
+    },
+    {
+        id: 4,
+        titolo: "SHOP",
+        sezioni: [
+            "Shop DC",
+            "Shop DC Collectibles",
+        ],
+    },
+];
 
 
 export default function Main() {
     return (
         <main>
-            <div className="container">
-                <h1 className="my-4">Content goes here</h1>
+            <div className="bg-dark text-white py-5">
+                <h4 className="ms-5 fw-bold mb-0">Content goes here</h4>
             </div>
             <Info />
+
+            <div className="bg-dc text-white py-5">
+                <div className="container ">
+                    <div className="row">
+
+                        {colonne.map((column) => (
+                            <div key={column.id} className="col-6 col-md-3">
+                                <h6 className="fw-bold">{column.titolo}</h6>
+                                <ul className="list-unstyled text-secondary small">
+                                    {column.sezioni.map((link, i) => (
+                                        <li key={i}>{link}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
+
+
+
         </main>
     )
 }
